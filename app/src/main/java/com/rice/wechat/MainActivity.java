5e2881e2e3dd4a4f45908d851e3e0b2a5146356a
ivy <wei512712@gmail.com>
2021-07-19 11:48:58 +0100

    初步連結firebase

--- app/src/main/java/com/rice/wechat/MainActivity.java
+++ app/src/main/java/com/rice/wechat/MainActivity.java
@@ -5,13 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     Toast.makeText(this,"git你好討厭",Toast.LENGTH_LONG);
+    mAuth=FirebaseAuth.getInstance();
     }
+    @Override
+    public void onStart(){
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+
+    }
+
+
 
 }
